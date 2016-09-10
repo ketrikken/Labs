67c80eaf7e02025a6bcffc4ad5c6eaac5ede8258
TesloGerc <yachmenev.mikhail@gmail.com>
2016-09-10 15:53:47 +0300

    Добавил координаты и кучу багов

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.AnswerLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 293);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // AnswerLabel
+            // 
+            this.AnswerLabel.AutoSize = true;
+            this.AnswerLabel.Location = new System.Drawing.Point(174, 300);
+            this.AnswerLabel.Name = "AnswerLabel";
+            this.AnswerLabel.Size = new System.Drawing.Size(113, 13);
+            this.AnswerLabel.TabIndex = 2;
+            this.AnswerLabel.Text = "Введите координаты";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(467, 339);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(20, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(468, 304);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(19, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 336);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(22, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(425, 301);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(21, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(378, 336);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(25, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(378, 301);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(24, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 371);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AnswerLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Л.Р. №1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label AnswerLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApplication1
+{
+    
+    public struct popo
+    {
+        public float x;
+        public float y;
+    }
+    public partial class Form1 : Form
+    {
+        bool IsCorrectData = false;
+        int countPens;
+        string[] color;
+        int step;
+        double corner;
+        popo[] points;
+        Graphics gr;       //объявляем объект - графику, на которой будем рисовать
+        Pen[] p;             //объявляем объект - карандаш, которым будем рисовать контур
+        SolidBrush fon;    //объявляем объект - заливки, для заливки соответственно фона
+        SolidBrush fig;    //и внутренности рисуемой фигуры
+        popo[] startPoints;
+        Random rand;      // объект, для получения случайных чисел
+        ///новые изменения, комит от 10,09,2016 @Миша
+        float firstXFactor = 0;
+        float firstYFactor = 0;
+        float secondXFactor = 0;
+        float secondYFactor = 0;
+
+        //
+        public Form1()
+        {
+            countPens = 0;
+            InitializeComponent();
+            step = 10;
+            corner = 15;
+            
+            startPoints = new popo[3];
+          /*  startPoints[0].x = 0;
+            startPoints[0].y = 0;
+            startPoints[1].x = 0;
+            startPoints[1].y = -30;
+            startPoints[2].x = 40;
+            startPoints[2].y = 0;*/
+            points = new popo[3];
+            points = startPoints;
+        }
+        private double Corner(double aa)
+        {
+            return aa * (Math.PI / 180);
+        }
+        private void DrawRectangle()
+        {
+            gr.DrawLine(p[countPens%3], points[0].x, points[0].y, points[1].x, points[1].y);
+            gr.DrawLine(p[countPens%3], points[0].x, points[0].y, points[2].x, points[2].y);
+            gr.DrawLine(p[countPens%3], points[1].x, points[1].y, points[2].x, points[2].y);
+        }
+        private void Shift()
+        {
+            popo[] tempPoints = points;
+            int tempStep = step;
+            bool flag = true;
+
+            while (flag == true)
+            {
+                points[0].x = startPoints[0].x + step;
+                points[0].y = (startPoints[0].y + (float)Math.Tan(Corner(corner)) * step);
+                points[1].x = points[0].x - firstXFactor;
+                points[1].y = points[0].y - firstYFactor;
+                points[2].x = points[0].x - secondXFactor;
+                points[2].y = points[0].y - secondYFactor;
+                flag = false;
+                for (int i = 0; i < 3; ++i)
+                {
+
+                    if (points[i].x >= ClientSize.Width || points[i].x <= 0)
+                    {
+                        countPens = ++countPens % 3;
+                        step *= (-1);
+                        corner *= (-1);
+                        flag = true;
+                        break;
+                    }
+                    if (points[i].y >= ClientSize.Height - 92 || points[i].y <= 0)
+                    {
+                        countPens = ++countPens % 3;
+                        corner *= (-1);
+                        flag = true;
+                        break;
+                    }
+
+                }
+            }
+
+            startPoints = tempPoints;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!IsRightData())
+            {
+                AnswerLabel.Text = "EROR!Again!";
+                IsCorrectData = false;
+            }
+            else
+            {
+                IsCorrectData = true;
+                AnswerLabel.Text = "OK";
+               // startPoints = new popo[3];
+
+                /*startPoints[0].x = Convert.ToInt32(textBox1.Text);
+                startPoints[0].y = int.Parse(textBox2.Text);
+                startPoints[1].x = int.Parse(textBox3.Text);
+                startPoints[1].y = int.Parse(textBox4.Text);
+                startPoints[2].x = int.Parse(textBox5.Text);
+                startPoints[2].y = int.Parse(textBox6.Text);*/
+                firstXFactor = startPoints[0].x - startPoints[1].x;
+                firstYFactor = startPoints[0].y - startPoints[1].y;
+                secondXFactor = startPoints[0].x - startPoints[2].x;
+                secondYFactor = startPoints[0].y - startPoints[2].y;
+
+                gr = pictureBox1.CreateGraphics();  //инициализируем объект типа графики
+                                                    // привязав  к PictureBox
+
+                p = new Pen[3];           // задали цвет для карандаша
+                p[0] = new Pen(Color.Green);
+                p[1] = new Pen(Color.Coral);
+                p[2] = new Pen(Color.FloralWhite);
+
+                fon = new SolidBrush(Color.Black); // и для заливки
+                fig = new SolidBrush(Color.Purple);
+
+                rand = new Random();               //инициализируем объект для рандомных числе
+
+                gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                timer1.Enabled = true;
+            }
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsCorrectData)
+            {
+                //сначала будем очищать область рисования цветом фона
+                gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                Shift();
+                DrawRectangle();
+            };
+
+        }
+
+        ///новые изменения, комит от 10,09,2016 @Миша
+        bool IsRightData()
+        {
+            int i = 0;
+            int j = 1;
+            foreach (TextBox c in Controls.OfType<TextBox>())
+            {
+                int result;
+                if (String.IsNullOrEmpty(c.Text) || !Int32.TryParse(c.Text, out result))
+                {
+
+                    return false;
+                }
+                else
+                {
+                    if (j % 2 == 1)
+                    {
+                        
+                        if (Int32.Parse(c.Text) < -9 || Int32.Parse(c.Text) >= 501)
+                            return false;
+                        else
+                            startPoints[i].x = Int32.Parse(c.Text);
+                        
+                    }
+                    else
+                    {
+                        if (Int32.Parse(c.Text) < -2 || Int32.Parse(c.Text) >= 250)
+                            return false;
+                        else
+                            startPoints[i].y = Int32.Parse(c.Text);
+                        ++i;
+                    }
+                    ++j;
+                }
+            }
+            return true;
+        }
+    }
+}
