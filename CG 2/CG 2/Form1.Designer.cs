096353581904254d8ab2d7a07205884a36e0b691
TesloGerc <yachmenev.mikhail@gmail.com>
2016-09-24 22:02:53 +0300

    Вторая лаба по КГ: сделал отрезок, осталось окружность

---
+++ CG 2/CG 2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CG_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.X_Box = new System.Windows.Forms.TextBox();
+            this.Y_Box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ans_line = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1132, 459);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // X_Box
+            // 
+            this.X_Box.Location = new System.Drawing.Point(264, 521);
+            this.X_Box.Name = "X_Box";
+            this.X_Box.Size = new System.Drawing.Size(37, 20);
+            this.X_Box.TabIndex = 2;
+            // 
+            // Y_Box
+            // 
+            this.Y_Box.Location = new System.Drawing.Point(362, 521);
+            this.Y_Box.Name = "Y_Box";
+            this.Y_Box.Size = new System.Drawing.Size(34, 20);
+            this.Y_Box.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 524);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 524);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y";
+            // 
+            // ans_line
+            // 
+            this.ans_line.AutoSize = true;
+            this.ans_line.Location = new System.Drawing.Point(236, 487);
+            this.ans_line.Name = "ans_line";
+            this.ans_line.Size = new System.Drawing.Size(205, 13);
+            this.ans_line.TabIndex = 6;
+            this.ans_line.Text = "Введите координаты точки в 5 октанте";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 573);
+            this.Controls.Add(this.ans_line);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Y_Box);
+            this.Controls.Add(this.X_Box);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox X_Box;
+        private System.Windows.Forms.TextBox Y_Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ans_line;
+    }
+}
+
---
+++ CG 2/CG 2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+namespace CG_2
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;       //We declare an object - graphics, which will draw
+        Pen p = new Pen(Color.Lime);            //We declare an object - a pencil, which will draw the contour
+        Pen penFon;
+        SolidBrush fon;    //We declare an object - fill, to fill the background respectively
+       
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color pix = Color.Chocolate;
+            gr = pictureBox1.CreateGraphics();  //initialize an object of type Graphics
+            fon = new SolidBrush(Color.Black);
+
+            gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);                                   // tied to a PictureBox
+            gr.DrawLine(p, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+            gr.DrawLine(p, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            Point Start_Coor = new Point(0, 0);
+            Point Point_Coor = new Point(Int32.Parse(X_Box.Text), Int32.Parse(Y_Box.Text));
+            if ((Point_Coor.X < 0) && (Point_Coor.Y > Point_Coor.X) && (Point_Coor.Y < 0))
+            {
+                SetLine(Start_Coor, Point_Coor);
+            }
+            else
+            {
+                ans_line.Text = "Некорректные данные!";
+            }
+            //PutPixel(Point_Coor.X, Point_Coor.Y);
+            
+            
+        }
+            
+        private void SetLine( Point start, Point finish)
+        {
+            int x = start.X;
+            int y = start.Y;
+            int dx = finish.X - start.X;
+            int dy = finish.Y - start.Y;
+            int D = -dx;
+            int DX = dx >> 1;
+            int DY = dy >> 1;
+            while (x > finish.X)
+            {
+                PutPixel(GetX(x), GetY(y));
+                --x;
+                D += DY;
+                if (D < 0)
+                {
+                    --y;
+                    D -= DX;
+                }
+            }
+        }
+        private void PutPixel(int x,int y)
+        {
+            gr.FillRectangle(Brushes.Azure, x, y, 1, 1);
+        }
+        private int GetX(int X)
+        {
+         
+            return X + pictureBox1.Width/2;
+        }
+        private int GetY(int Y)
+        {
+           
+            return pictureBox1.Height/2-Y;
+        }
+
+
+
+    }
+}
