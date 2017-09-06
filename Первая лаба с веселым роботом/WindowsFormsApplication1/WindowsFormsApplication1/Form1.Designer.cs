edef10d10dadf1f535fbd4ce3a15b9a4c348f1a7
ketrikken <ketrikken.ann@gmail.com>
2017-09-06 15:59:31 +0300

    real-time update textBoxes

--- Первая лаба с веселым роботом/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Первая лаба с веселым роботом/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -75,6 +75,7 @@
             this.textBox48 = new System.Windows.Forms.TextBox();
             this.textBox49 = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -437,11 +438,22 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 521);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.textBox38);
             this.Controls.Add(this.textBox39);
@@ -546,6 +558,7 @@
         private System.Windows.Forms.TextBox textBox48;
         private System.Windows.Forms.TextBox textBox49;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Первая лаба с веселым роботом/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Первая лаба с веселым роботом/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -11,23 +11,30 @@ using System.Threading;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Text;
-
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-
+        bool flagClose = false;
+        private byte[] data = new byte[1024];
+        private int size = 1024;
+        private Socket client;
         TextBox[] textBoxes;
-      
-        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
-        NetworkStream serverStream = default(NetworkStream);
-        string readData = null;
+
+        //System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+       // NetworkStream serverStream = default(NetworkStream);
+        //string readData = null;
 
         public Form1()
         {
             InitializeComponent();
-            textBoxes = new[] {textBox3, textBox4, textBox8, textBox6, textBox16, textBox14, textBox12, textBox10, textBox32, textBox30, textBox28, textBox26, textBox24, textBox22, textBox20, textBox18, textBox48 };
+            textBoxes = new[] { textBox3, textBox4, textBox8, textBox6, textBox16, textBox14, textBox12, textBox10, textBox32, textBox30, textBox28, textBox26, textBox24, textBox22, textBox20, textBox18, textBox48 };
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,36 +44,68 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
 
-            GetDATA();
+            //GetDATA();
         }
 
-        private void GetDATA()
-        {
-            serverStream = clientSocket.GetStream();
-            Byte[] data = new Byte[256];
-            String responseData = String.Empty;
-            Int32 bytes = serverStream.Read(data, 0, data.Length);
-            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-            textBox1.Text += "     " + responseData;
-            Parse(responseData);
-        }
+        //private void GetDATA()
+        //{
+        //    serverStream = clientSocket.GetStream();
+        //    Byte[] data = new Byte[256];
+        //    String responseData = String.Empty;
+        //    Int32 bytes = serverStream.Read(data, 0, data.Length);
+        //    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+        //    textBox1.Text += "     " + responseData;
+        //    Parse(responseData);
+        //}
 
-        private void msg()
-        {
-            if (this.InvokeRequired)
-                this.Invoke(new MethodInvoker(msg));
-            else
-                textBox1.Text = readData;
-        }
+        //private void msg()
+        //{
+        //    if (this.InvokeRequired)
+        //        this.Invoke(new MethodInvoker(msg));
+        //    else { }
+        //        //textBox1.Text = readData;
+        //}
 
-        
+
 
         private void button2_Click(object sender, EventArgs e)
         {
-            clientSocket.Connect("127.0.0.1", 9999);
-            serverStream = clientSocket.GetStream();
+            // clientSocket.Connect("127.0.0.1", 9999);
+            // serverStream = clientSocket.GetStream();
+            flagClose = false;
+            Socket newsock = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            newsock.BeginConnect(iep, new AsyncCallback(Connected), newsock);
         }
-
+        void Connected(IAsyncResult iar)
+        {
+            client = (Socket)iar.AsyncState;
+            try
+            {
+                client.EndConnect(iar);
+                textBox1.Text = "Connected to: " + client.RemoteEndPoint.ToString();
+                client.BeginReceive(data, 0, size, SocketFlags.None,
+                              new AsyncCallback(ReceiveData), client);
+            }
+            catch (SocketException)
+            {
+                textBox1.Text = "Error connecting";// просто что бы видеть если ошибка соединения
+            }
+        }
+        void ReceiveData(IAsyncResult iar)
+        {
+            Socket remote = (Socket)iar.AsyncState;
+            int recv = remote.EndReceive(iar);
+            string stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Parse(stringData);
+            if (flagClose != true)
+            {
+                client.BeginReceive(data, 0, size, SocketFlags.None,
+                             new AsyncCallback(ReceiveData), client);
+            }
+            
+        }
         private void Parse(String inputString)
         {
             List<String> stringList = new List<string>();
@@ -77,7 +116,8 @@ namespace WindowsFormsApplication1
                 {
                     stringList.Add(temp);
                     temp = String.Empty;
-                }else if (Char.IsDigit(inputString[i]) || inputString[i] == '.')
+                }
+                else if (Char.IsDigit(inputString[i]) || inputString[i] == '.')
                 {
                     temp += inputString[i];
                 }
@@ -97,6 +137,12 @@ namespace WindowsFormsApplication1
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            flagClose = true;
+            client.Close();
         }
     }
 }
